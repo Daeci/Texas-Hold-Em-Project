aa1c79cd7b5961fd30e7fc6c952eaf1ffd1dba66
Daeci <47134852+Daeci@users.noreply.github.com>
2019-04-01 11:57:56 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -4,3 +4,21 @@ std::ostream& operator<<(std::ostream &os, const Player& p)
 {
     os << "Player " << p.playerNumber << std::endl;
 }
+
+void Player::humanRaise(int maxBet)
+{
+    std::cout << "Remaining money: " << money;
+    int raiseAmount = 0;
+    do {
+        std::cout << "Raise amount: ";
+        std::cin >> raiseAmount;
+        if (raiseAmount <= (maxBet - betAmount)) {
+            std::cout << "Raise needs to be higher than the max bet!\n";
+        }
+    } while (raiseAmount <= (maxBet - betAmount));
+}
+
+void Player::computerRaise()
+{
+
+}
