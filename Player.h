7fba39086d176a372e263b5a7c3b9d80c4e9019b
Daeci <47134852+Daeci@users.noreply.github.com>
2019-04-01 02:29:54 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -7,17 +7,25 @@ private:
     Card c2;
     int money;
     int playerNumber;
-    bool isHuman;
-    bool isBigBlind;
-    bool isSmallBlind;
+    bool human;
+    bool folded;
+    bool bigBlind;
+    bool smallBlind;
 public:
     Player() { /* place holder */ }
     Player(Card c1, Card c2, int startingMoney) { this->c1 = c1; this->c2 = c2; money = startingMoney; }
     Card getCard1() { return c1; }
     Card getCard2() { return c2; }
-    void setHuman(bool b) { isHuman = b; }
+    void setHuman(bool b) { human = b; }
     void setPlayerNumber(int num) { playerNumber = num; }
-    void setBigBlind(bool b) { isBigBlind = b; }
-    void setSmallBlind(bool b) { isSmallBlind = b; }
+    void setBigBlind(bool b) { bigBlind = b; }
+    void setSmallBlind(bool b) { smallBlind = b; }
+    void setCards(Card c1, Card c2) { this->c1 = c1; this->c2 = c2; }
+    void setMoney(int money) { this->money = money; }
+    void changeMoney(int money) { this->money += money; }
+    void setFolded(bool b) { folded = b; }
+    bool isHuman() { return human; }
+    bool isFolded() { return folded; }
+    int getMoney() { return money; }
     friend std::ostream& operator<<(std::ostream &os, const Player&);
 };
