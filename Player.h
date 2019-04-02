7c5fd767b0ba804e06d44b755dafea014ea25183
Daeci <47134852+Daeci@users.noreply.github.com>
2019-04-02 00:05:17 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -10,6 +10,8 @@ private:
     int betAmount;
     bool human;
     bool folded;
+    bool checked;
+    bool allin;
     bool bigBlind;
     bool smallBlind;
 public:
@@ -22,16 +24,24 @@ public:
     void setSmallBlind(bool b) { smallBlind = b; }
     void setCards(Card c1, Card c2) { this->c1 = c1; this->c2 = c2; }
     void setMoney(int money) { this->money = money; }
+    void setPlayerNumber(int number) { playerNumber = number; }
+    void setChecked(bool b) { checked = b; }
+    void setAllin(bool b) { allin = b; }
     void changeMoney(int money) { this->money += money; }
     void initFolded() { folded = false; }
+    void initChecked() { checked = false; }
     void initBetAmount() { betAmount = 0; }
+    void initAllin() { allin = false; }
     void changeBetAmount(int amount) { betAmount += amount; }
     bool isHuman() { return human; }
     bool isFolded() { return folded; }
+    bool isChecked() { return checked; }
+    bool isAllin() { return allin; }
     int getMoney() { return money; }
     int getBetAmount() { return betAmount; }
     friend std::ostream& operator<<(std::ostream &os, const Player&);
     void fold() { folded = true; }
-    void humanRaise(int maxBet);
-    void computerRaise();
+    void check() { checked = true; }
+    int humanRaise(int maxBet);
+    int computerRaise(int maxBet);
 };
