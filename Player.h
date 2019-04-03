48876e3089f7739a26884adda8f51629304c39d0
Daeci <47134852+Daeci@users.noreply.github.com>
2019-04-02 21:19:25 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,3 +1,7 @@
+/*
+ * Created by: Sascha Scheidegger and Mitchell Caughron
+ */
+
 #pragma once
 #include "Card.h"
 
@@ -5,33 +9,31 @@ class Player {
 private:
     Card c1;
     Card c2;
-    int money;
-    int playerNumber;
-    int betAmount;
-    bool human;
-    bool folded;
-    bool checked;
-    bool allin;
-    bool bigBlind;
-    bool smallBlind;
+    int money{ 0 };
+    int playerNumber{ 0 };
+    int betAmount{ 0 };
+    bool human{ false };
+    bool folded{ false };
+    bool checked{ false };
+    bool allin{ false };
+    bool bigBlind{ false };
+    bool smallBlind{ false };
 public:
     Player() { /* place holder */ }
     Player(Card c1, Card c2, int startingMoney) { this->c1 = c1; this->c2 = c2; money = startingMoney; }
+    ~Player() { /* place holder */ }
     Card getCard1() { return c1; }
     Card getCard2() { return c2; }
     void setHuman(bool b) { human = b; }
-    void setBigBlind(bool b) { bigBlind = b; }
-    void setSmallBlind(bool b) { smallBlind = b; }
+    void setBigBlind() { bigBlind = true; }
+    void setSmallBlind() { smallBlind = true; }
     void setCards(Card c1, Card c2) { this->c1 = c1; this->c2 = c2; }
     void setMoney(int money) { this->money = money; }
     void setPlayerNumber(int number) { playerNumber = number; }
     void setChecked(bool b) { checked = b; }
     void setAllin(bool b) { allin = b; }
+    void resetBlinds() { bigBlind = false; smallBlind = false; }
     void changeMoney(int money) { this->money += money; }
-    void initFolded() { folded = false; }
-    void initChecked() { checked = false; }
-    void initBetAmount() { betAmount = 0; }
-    void initAllin() { allin = false; }
     void changeBetAmount(int amount) { betAmount += amount; }
     bool isHuman() { return human; }
     bool isFolded() { return folded; }
