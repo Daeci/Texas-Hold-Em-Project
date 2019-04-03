75662591ab0fefbbfcbcbe65e07aca9f017cc6e2
Daeci <47134852+Daeci@users.noreply.github.com>
2019-04-03 00:18:51 -0700

    Delete debug.h

--- debug.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector> // std::vector<>
-#include "Player.h"
-
-void debugDeckDisplay(std::vector<Card> deck);
-void debugPlayerCardsDisplay(Player p, std::string name);
-void debugRiverDeckDisplay(std::vector<Card> riverDeck);
